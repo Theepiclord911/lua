57886129b8bbfb6a8eb53c3233958225fc57ef9c
Theepiclord911 <128190523+Theepiclord911@users.noreply.github.com>
2024-08-27 14:11:54 +0200

    Update RemoveShake.lua
    This seemed to work better. i thought it would crash the game due to too many inputs but it works nicely for me

--- RemoveShake.lua
+++ RemoveShake.lua
@@ -1,47 +1,9 @@
-client.RemoveConVarProtection("sv_cheats") --bypass security(bypass sv_cheats check)
+client.RemoveConVarProtection("sv_cheats") -- bypass security (bypass sv_cheats check)
 client.SetConVar("sv_cheats", 1, true)
-local dist_pipebomb = 300 -- how close the projectile needs to be for shake_stop to be triggered (hammer units)
-local dist_energyball = 700 -- how close the energy ball needs to be for shake_stop to be triggered (hammer units)
-local shake_duration = 0.1 -- duration to continue spamming shake_stop after projectile is gone (seconds)
 
-local projectile_class_names = {
-    ["CTFGrenadePipebombProjectile"] = dist_pipebomb,
-    ["CTFProjectile_EnergyBall"] = dist_energyball
-}
-
-local projectile_timestamps = {}
-
-local function CalculateDistance(vec1, vec2)
-    local dx, dy, dz = vec1.x - vec2.x, vec1.y - vec2.y, vec1.z - vec2.z
-    return math.sqrt(dx * dx + dy * dy + dz * dz)
+local function StopScreenShake(cmd)
+    client.Command("shake_stop", true)
 end
 
-local function IsProjectileClose(lPlayer, projectile, min_distance)
-    return CalculateDistance(lPlayer:GetAbsOrigin(), projectile:GetAbsOrigin()) < min_distance
-end
-
-local function SpamShakeStop(cmd)
-    local lPlayer = entities.GetLocalPlayer()
-    if not lPlayer then return end
-
-    local current_time = globals.CurTime()
-
-    for class_name, min_distance in pairs(projectile_class_names) do
-        for _, p in pairs(entities.FindByClass(class_name)) do 
-            if not p:IsDormant() and IsProjectileClose(lPlayer, p, min_distance) then
-                projectile_timestamps[p:GetIndex()] = current_time
-            end
-        end
-    end
-
-    for index, timestamp in pairs(projectile_timestamps) do
-        if current_time - timestamp < shake_duration then
-            client.Command("shake_stop", true)
-        else
-            projectile_timestamps[index] = nil
-        end
-    end
-end
-
-callbacks.Unregister("CreateMove", "SpamShakeStop")
-callbacks.Register("CreateMove", "SpamShakeStop", SpamShakeStop)
+callbacks.Unregister("CreateMove", "StopScreenShake")
+callbacks.Register("CreateMove", "StopScreenShake", StopScreenShake)
